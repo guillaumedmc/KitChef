b3bbb552e0a1c1fc8a6a8939d96db988f5853c20
Guillaume de Montecler <guillaume.demontecler@gmail.com>
2019-02-18 17:04:53 +0100

    db créee

--- Gemfile
+++ Gemfile
@@ -18,6 +18,8 @@ gem 'webpacker'
 
 gem 'devise'
 
+gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
+
 group :development do
   gem 'web-console', '>= 3.3.0'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'faker'
+
+# user = User.new(
+#   email: Faker::Internet.email,
+#   password: 'azerty',
+#   first_name: Faker::Name.name,
+#   last_name: Faker::Name.name,
+#   address: Faker::Address.street_address,
+#   chef: false
+# )
+# user.save!
+
+# product = Product.new(
+#   name: Faker::Food.dish,
+#   user_id: user
+#   )
+# product.save!
+
+10.times do
+  user = User.new(
+    email: Faker::Internet.email,
+    password: 'azerty',
+    first_name: Faker::Name.name,
+    last_name: Faker::Name.name,
+    address: Faker::Address.street_address,
+    chef: false
+    )
+    user.save!
+    5.times do
+      product = Product.new(
+        name: Faker::Food.dish,
+        user_id: user.id
+        )
+      product.save!
+    end
+end
+
