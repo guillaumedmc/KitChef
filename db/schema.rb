65e3fd2f6f93e3ff048b8f2fc548964a2e5f326d
Guillaume de Montecler <guillaume.demontecler@gmail.com>
2019-02-18 17:59:20 +0100

    ajout bookings model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_141247) do
+ActiveRecord::Schema.define(version: 2019_02_18_164619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "product_id"
+    t.bigint "user_id"
+    t.integer "covers"
+    t.boolean "status"
+    t.date "date"
+    t.integer "total_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_bookings_on_product_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -43,5 +56,7 @@ ActiveRecord::Schema.define(version: 2019_02_18_141247) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "products"
+  add_foreign_key "bookings", "users"
   add_foreign_key "products", "users"
 end
