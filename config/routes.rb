c75c48d36121b258ece63f92c01316af62d0dddb
Guillaume de Montecler <guillaume.demontecler@gmail.com>
2019-02-18 18:28:43 +0100

    booking en cours

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
    resources :products do
     resources :bookings, only: [ :index ]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
