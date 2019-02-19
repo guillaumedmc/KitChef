0d630d42fec722b2f2b6f904937671981fc4b613
Pierre Francois de Rouge <pifoupif@hotmail.com>
2019-02-19 15:11:08 +0100

    pull request for upload picture

---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,23 @@
+class BookingPolicy < ApplicationPolicy
+
+  def create?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
+
+
+
