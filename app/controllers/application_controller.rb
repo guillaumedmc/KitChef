3462d607b493fd8d01796c20dbcdfeadfe241263
Guillaume de Montecler <guillaume.demontecler@gmail.com>
2019-02-19 15:12:44 +0100

    form de sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,13 @@ class ApplicationController < ActionController::Base
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address])
+  end
+
   private
 
   def skip_pundit?
---
+++ app/policies/user_policy.rb
@@ -0,0 +1,11 @@
+class UserPolicy < ApplicationPolicy
+  def show?
+    true
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
