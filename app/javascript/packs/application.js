f1fa021cd9be856616e8aa895688f21a948afd3e
pierre-francois <pifoupif@hotmail.com>
2019-02-22 11:59:05 +0100

    dynamic price done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,12 @@
 import "bootstrap";
 import { calendar } from '../components/calendar';
+import { price } from '../components/price';
 
 
 if (document.querySelector("grid-calendar")) {
   calendar();
+}
+
+if (document.getElementById("show-page")) {
+  price();
 }
